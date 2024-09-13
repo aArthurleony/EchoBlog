2befe21e8bfa43742d977306b7f270896b10f73e
Wendel <wendel824829has@gmail.com>
2024-09-13 08:31:25 -0300

    controller e router de rf03

--- backend/src/controllers/usuarioController.js
+++ backend/src/controllers/usuarioController.js
@@ -39,3 +39,42 @@ export const cadastrarUsuario = async (request, response) => {
     response.status(500).json({ error: "erro ao cadastrar usuario" });
   }
 };
+export const atualizarUsuario = async (request, response) => {
+  // const paramValidator = getSchema.safeParse(request.params);
+  // if (!paramValidator.success) {
+  //   response.status(400).json({
+  //     message: "numero de identicação está inválido",
+  //     detalhes: formatZodError(paramValidator),
+  //   });
+  //   return;
+  // }
+  const updateValidator = updateSchema.safeParse(request.body);
+  if (!updateValidator.success) {
+    response.status(400).json({
+      message: "dados para atualização estão incorretos",
+      details: formatZodError(updateValidator.error),
+    });
+    return;
+  }
+  const { id } = request.params;
+  const { nome, email, senha } = request.body;
+  const usuarioAtualizado = {
+    nome,
+    email,
+    senha,
+  };
+  try {
+    const [linhasAfetadas] = await Usuario.update(usuarioAtualizado, {
+      where: { id },
+    });
+    if (linhasAfetadas === 0) {
+      response.status(404).json({ message: "usuario não encontrado" });
+      return;
+    }
+    response.status(200).json({
+      message: "usuario atualizado",
+    });
+  } catch (error) {
+    response.status(500).json({ message: "erro ao atualizar usuario" });
+  }
+};
--- backend/src/routes/usuariosRouter.js
+++ backend/src/routes/usuariosRouter.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { cadastrarUsuario } from "../controllers/usuarioController.js";
+import {
+  cadastrarUsuario,
+  atualizarUsuario,
+} from "../controllers/usuarioController.js";
 
 const router = Router();
 
 router.post("/", cadastrarUsuario);
+router.put("/:id", atualizarUsuario);
 
 export default router;
